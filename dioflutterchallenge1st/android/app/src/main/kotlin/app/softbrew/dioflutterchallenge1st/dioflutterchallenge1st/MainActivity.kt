e6f875aaace1b4864010a178db0bc139095225d0
Wilker Foureaux <wilker.foureaux@gmail.com>
2023-09-15 07:05:11 -0300

    flutter works

---
+++ dioflutterchallenge1st/android/app/src/main/kotlin/app/softbrew/dioflutterchallenge1st/dioflutterchallenge1st/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.softbrew.dioflutterchallenge1st.dioflutterchallenge1st
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dioflutterchallenge1st/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Colors.yellowAccent.shade700),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Wilker Dio Flutter 1st Challenge'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('images/dio-logo-full.png'),
+            Text(''),
+            const Text(
+              'You have pushed:',
+            ),
+            Text(
+              '$_counter ${_counter == 0 ? "time" : "times"}',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.fingerprint),
+      ),
+    );
+  }
+}
